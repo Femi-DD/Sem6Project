29340d2772ddc627328aae9358173e6722d29fb1
ScottzMC <scottmike275@gmail.com>
2018-03-22 23:18:39 +0100

    Add files via upload

--- aptech.sql
+++ aptech.sql
@@ -1,22 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.2.11
+-- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Mar 04, 2018 at 09:33 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Host: 127.0.0.1
+-- Generation Time: Mar 22, 2018 at 07:58 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `aptech`
@@ -25,17 +23,89 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contact_details`
+--
+
+CREATE TABLE IF NOT EXISTS `contact_details` (
+`id` int(11) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telephone` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `postcode` varchar(10) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact_details`
+--
+
+INSERT INTO `contact_details` (`id`, `firstname`, `lastname`, `email`, `telephone`, `address`, `postcode`, `region`, `city`) VALUES
+(1, 'Susu Ali', 'Kilmonger', 'wakanda@fever.com', '419270419', 'Hidden in the Clouds, Off Sky and Moon, Galaxy', '1000010', 'Bitcoin Valley', 'Lagos');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
+
+CREATE TABLE IF NOT EXISTS `property` (
+`id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `payment` varchar(20) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `property`
+--
+
+INSERT INTO `property` (`id`, `title`, `price`, `type`, `payment`, `region`, `state`) VALUES
+(1, 'New House Winged Duplex', '50000000', 'Residential Land', 'Bitcoin', 'Bitcoin Valley', 'Lagos');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property_details`
+--
+
+CREATE TABLE IF NOT EXISTS `property_details` (
+`id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `bedroom` varchar(10) NOT NULL,
+  `bathroom` varchar(10) NOT NULL,
+  `interior` varchar(10) NOT NULL,
+  `facility` text NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `property_details`
+--
+
+INSERT INTO `property_details` (`id`, `title`, `description`, `bedroom`, `bathroom`, `interior`, `facility`, `image`) VALUES
+(1, 'New House Winged Duplex', 'Its a new house that is available in the Dark Web for all those thieves and hackers.', '4', '6', 'Yes', 'Oven/Range,Freezer', 'apartment.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL,
   `name` varchar(500) NOT NULL,
   `username` varchar(100) NOT NULL,
   `email` varchar(500) NOT NULL,
   `password` varchar(500) NOT NULL,
   `signup_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -49,22 +119,53 @@ INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `signup_date
 --
 
 --
+-- Indexes for table `contact_details`
+--
+ALTER TABLE `contact_details`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `property`
+--
+ALTER TABLE `property`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `property_details`
+--
+ALTER TABLE `property_details`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `contact_details`
+--
+ALTER TABLE `contact_details`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `property`
+--
+ALTER TABLE `property`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `property_details`
+--
+ALTER TABLE `property_details`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
